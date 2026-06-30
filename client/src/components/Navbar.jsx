f3bde355653bc49d06422c8decda19ccda0dda50
“Peehal” <sinhapeehal@gmail.com>
2026-06-30 09:28:56 +0530

    Add personal info input fields and fix global checkbox CSS typo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     
     <div className='shadow bg-white'>
         <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
-            <Link>
+            <Link >
             <img src={logo} alt='logo' className='h-11 w-auto' />
             </Link>
 
--- client/src/components/PersonalInfoForm.jsx
+++ client/src/components/PersonalInfoForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { User } from 'lucide-react'
+import { BriefcaseBusiness, Mail, MapPin, Phone, User, Globe } from 'lucide-react'
 
 
 
@@ -9,6 +9,14 @@ const PersonalInfoForm = ({data, onChange, removeBackground, setRemoveBackground
     onChange({...data, [field]: value})
   }
 
+  const fields =[
+    {key:"full_name", label:"Full Name", icon: User, type:"text", required:true}, 
+    {key:"email", label:"Email Address", icon: Mail, type:"email", required:true}, 
+    {key:"phone", label:"Phone Number", icon: Phone, type:"tel", required:true}, 
+    {key:"location", label:"Location", icon: MapPin, type:"text", required:true}, 
+    {key:"profession", label:"Profession", icon: BriefcaseBusiness, type:"text", required:true},
+    {key:"website", label:"Personal Website", icon: Globe, type:"url", required:true}, 
+  ]
 
   return (
     <div>
@@ -37,6 +45,25 @@ const PersonalInfoForm = ({data, onChange, removeBackground, setRemoveBackground
             </div>
           )}
       </div>
+      
+      {fields.map((field) =>{
+        const Icon = field.icon;
+        return(
+          <div key={field.key} className='space-y-1 mt-5'>
+            <label className='flex items-center gap-2 text-sm font-medium text-gray-600'>
+              <Icon className='size-4'/>
+              {field.label}
+              {field.required && <span className='text-red-500'>*</span>}
+            </label>
+
+            <input type={field.type} value={data[field.key] || ""} 
+            
+            onChange={(e) => handleChange(field.key, e.target.value)}
+            className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors text-sm'
+            placeholder={`Enter your ${field.label.toLowerCase()}`} required={field.required}  />
+          </div>
+        )
+      })}
     </div>
   )
 }
