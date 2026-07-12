cbcea828dc88b3e27e00994827e75449b2211dde
“Peehal” <sinhapeehal@gmail.com>
2026-07-12 11:36:56 +0530

    feat(builder): add SkillsForm with tag-based input and wire up skills section with Save Changes button

--- client/src/pages/ResumeForge.jsx
+++ client/src/pages/ResumeForge.jsx
@@ -10,6 +10,7 @@ import { ProfessionalSummaryForm } from '../components/ProfessionalSummaryForm';
 import ExperienceForm from '../components/ExperienceForm';
 import EducationForm from '../components/EducationForm';
 import ProjectForm from '../components/ProjectForm';
+import SkillsForm from '../components/SkillsForm';
 
 const ResumeForge = () => {
   const { resumeId } = useParams();
@@ -119,8 +120,15 @@ const ResumeForge = () => {
                         {activeSection.id === 'projects' &&(
                           <ProjectForm data={resumeData.project} onChange={(data) => setResumeData(prev =>({...prev, project:data} ))}  />
                         )}
+
+                        {activeSection.id === 'skills' &&(
+                          <SkillsForm data={resumeData.skills} onChange={(data) => setResumeData(prev =>({...prev, skills:data} ))}  />
+                        )}
                         
                       </div>
+                      <button className='bg-linear-to-br from-indigo-100 to indigo-200 ring-indigo-300 text-indigo-600 ring hover:ring-indigo-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+                        Save Changes
+                      </button>
                      </div>
 
                 </div>
